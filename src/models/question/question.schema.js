7ba1832eeab5b9023d46c8e001e162c9a80a5386
turkishdevelop <abdullahsevmez@gmail.com>
2022-05-19 17:25:32 +0300

    question.schema.js tamamlandı. soru modelinin şeması oluşturuldu.

--- src/models/question/question.schema.js
+++ src/models/question/question.schema.js
@@ -2,7 +2,41 @@
 const mongoose = require('mongoose');
 
 //! questionSchema oluşturuldu.
-const questionSchema = mongoose.Schema({});
+const questionSchema = mongoose.Schema({
+  //! Sorunun gösterileceği metin
+  title: {
+    type: String,
+    required: true,
+  },
+
+  //! Cevap Listesi
+  answerList: {
+    type: [String],
+    required: true,
+  },
+  //! Doğru cevabın indexi
+  correctAnswerIndex: {
+    type: Number,
+    required: true,
+  },
+
+  //! Sorunun toplam görüntülenme sayısı
+  seenCount: {
+    type: Number,
+    default: 0,
+  },
+  //! Sorunun kategorisi
+  category: {
+    type: String,
+    required: true,
+  },
+  //! Sorunun tipi - test ya da  drawable olabilir. Matematik ve Geometri sorularının ekranda çözülebilmesi için gerekli.
+  type: {
+    type: String,
+    require: true,
+    default: 'test',
+  },
+});
 
 //! questionSchema yı module.exports ile dışarı çıkarıyoruz.
 module.exports = mongoose.model('Question', questionSchema);
