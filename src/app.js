ee849a79ca64f9fed80bf4804d4d07aaa9a387ec
Abdullah Sevmez <39024317+sevmezabdullah@users.noreply.github.com>
2022-10-31 15:09:29 +0300

    test

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const config = require('./config/config');
 //+ Routing işlemlerini daha rahat yapabilmek amacıyla api.js dosyası import edilir.
 const api = require('./routes/api');
-
+// data eklendi
 //+ eskiden request body sinde gelen json dosyalarını decode etmek için farklı paketlere ihtiyaç duyulur. Ancak express e gelen güncellemelerle alttaki kod ile gelen requestlerin bodylerini json olarak direk alabiliyoruz.
 app.use(express.json());
 //+ routing yapmak için app.use middleware metoduna apiyi parametre olarak geçiyoruz.
